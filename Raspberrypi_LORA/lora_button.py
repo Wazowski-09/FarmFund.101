d72d5ada93c62e9a26c9ea9f042647d95e3a590e
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-08 10:37:44 +0700

    lora

--- Raspberrypi_LORA/lora_button.py
+++ Raspberrypi_LORA/lora_button.py
@@ -8,6 +8,17 @@ BOARD.setup()
 BOARD.reset()
 #parser = LoRaArgumentParser("Lora tester")
 
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Setup event on pin 10 rising edge
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+
+message = input("Press enter to quit\n\n")  # Run until someone presses enter
+
+GPIO.cleanup()  # Clean up
 
 class mylora(LoRa):
     def __init__(self, verbose=False):
@@ -88,18 +99,6 @@ lora.set_low_data_rate_optim(True)
 #  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
 # lora.set_pa_config(pa_select=1)
 
-
-GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# Setup event on pin 10 rising edge
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
-
-message = input("Press enter to quit\n\n")  # Run until someone presses enter
-
-GPIO.cleanup()  # Clean up
 
 assert(lora.get_agc_auto_on() == 1)
 
