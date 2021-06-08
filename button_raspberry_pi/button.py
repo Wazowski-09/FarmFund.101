05cc728e3e21cb933b72bacbac090b3f917c893a
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-08 11:13:51 +0700

    lora

--- button_raspberry_pi/button.py
+++ button_raspberry_pi/button.py
@@ -5,6 +5,8 @@ class button():
     def button_callback(channel):
         print("Button was pushed!")
 
+class_instance = button()
+
 
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
@@ -12,7 +14,7 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Setup event on pin 10 rising edge
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(10, GPIO.RISING, callback=class_instance.button_callback)
 
 # message = input("Press enter to quit\n\n")  # Run until someone presses enter
 
