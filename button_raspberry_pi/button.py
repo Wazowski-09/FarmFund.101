1528ddeafd3b150f6eeaac69b90cc3f40fef14fb
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-08 10:45:59 +0700

    lora

--- button_raspberry_pi/button.py
+++ button_raspberry_pi/button.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
 
-def button_callback(channel):
-    print("Button was pushed!")
+class button():
+    def button_callback(channel):
+        print("Button was pushed!")
 
 
 GPIO.setwarnings(False)  # Ignore warning for now
@@ -13,6 +14,17 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Setup event on pin 10 rising edge
 GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
-#message = input("Press enter to quit\n\n")  # Run until someone presses enter
+# message = input("Press enter to quit\n\n")  # Run until someone presses enter
 
 GPIO.cleanup()  # Clean up
+
+try:
+    print("START button")
+    button.start()
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    print("Exit")
+    sys.stderr.write("KeyboardInterrupt\n")
+finally:
+    sys.stdout.flush()
+    print("Exit")
