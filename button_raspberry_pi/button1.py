1f195fabd38ab995e33bf83e67e75d8261b0174a
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-08 11:25:47 +0700

    lora

--- button_raspberry_pi/button1.py
+++ button_raspberry_pi/button1.py
@@ -3,6 +3,7 @@ GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:
