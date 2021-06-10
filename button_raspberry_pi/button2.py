057769d60b71b5b8b68dec50609cc56421cff205
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-10 10:50:10 +0700

    lora

--- button_raspberry_pi/button2.py
+++ button_raspberry_pi/button2.py
@@ -4,6 +4,9 @@ GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:  # Run forever
     if GPIO.input(38) == GPIO.HIGH:
-        print("Button was pushed!")
+        print("Button off!")
+    elif GPIO.input(40) == GPIO.HIGH:
+        print("Button on!")
