831ad43b454bbc92189bd67d35f88a43ebd8f2b3
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-10 11:18:05 +0700

    lora

--- button_raspberry_pi/button1.py
+++ button_raspberry_pi/button1.py
@@ -1,19 +1,12 @@
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-#GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# this is only for input pins
-GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW, pull_up_down=GPIO.PUD_UP)
-
-#CORRECT ("initial" is optional)
-#GPIO.setup(n, RPIO.OUT, initial=RPIO.LOW)
-
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:  # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
+    if GPIO.input(38) == GPIO.HIGH:
         print("Button off!")
-    #elif GPIO.input(14) == GPIO.HIGH:
-     #   print("Button on!")
+    elif GPIO.input(40) == GPIO.HIGH:
+        print("Button on!")
