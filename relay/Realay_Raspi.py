c47ebed75ab63d9a57dcab34f8d96d80826a5c99
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-10 16:23:07 +0700

    lora

--- relay/Realay_Raspi.py
+++ relay/Realay_Raspi.py
@@ -9,7 +9,7 @@ GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
 while True:
     print("start")
     GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
-    time.sleep(3)
+    time.sleep(1)
     print("stop")
     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
-    time.sleep(3)
+    time.sleep(1)
