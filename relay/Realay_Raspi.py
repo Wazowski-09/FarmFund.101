cd4a38c5acc727461a62f78ca669f78bc2253bbb
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-10 16:02:20 +0700

    lora

--- relay/Realay_Raspi.py
+++ relay/Realay_Raspi.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
- 
+GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
+
 RELAIS_1_GPIO = 16
-GPIO.setwarnings(False) 
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-time.sleep(3)
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-time.sleep(3)
+GPIO.setwarnings(False)
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
+
+while True:
+    GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
+    time.sleep(3)
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
+    time.sleep(3)
