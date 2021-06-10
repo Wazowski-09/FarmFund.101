e73db2c8533889dec12f91454193ec32ccddfd4b
wazowski-09 <mike.wazowski.922002@gmail.com>
2021-06-10 12:21:39 +0700

    lora

--- button_raspberry_pi/button2.py
+++ button_raspberry_pi/button2.py
@@ -1,112 +1,14 @@
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
-import time
-from SX127x.LoRa import *
-#from SX127x.LoRaArgumentParser import LoRaArgumentParser
-from SX127x.board_config import BOARD
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-BOARD.setup()
-BOARD.reset()
+def button_callback(channel):
+    print("Button was pushed!")
 
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-class mylora(LoRa):
-    def __init__(self, verbose=False):
-        super(mylora, self).__init__(verbose)
-        self.set_mode(MODE.SLEEP)
-        self.set_dio_mapping([0] * 6)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
-    def on_rx_done(self):
-        BOARD.led_on()
-        # print("\nRxDone")
-        self.clear_irq_flags(RxDone=1)
-        payload = self.read_payload(nocheck=True)  # Receive INF
-        print("Receive: ")
-        mens = bytes(payload).decode("utf-8", 'ignore')
-        mens = mens[2:-1]  # to discard \x00\x00 and \x00 at the end
-        print(mens)
-        BOARD.led_off()
-        if mens == "INF":
-            print("Received data request INF")
-            time.sleep(2)
-            print("Send mens: DATA RASPBERRY PI")
-            self.write_payload([255, 255, 0, 0, 68, 65, 84, 65, 32, 82, 65, 83,
-                                80, 66, 69, 82, 82, 89, 32, 80, 73, 0])  # Send DATA RASPBERRY PI
-            self.set_mode(MODE.TX)
-        time.sleep(2)
-        self.reset_ptr_rx()
-        self.set_mode(MODE.RXCONT)
+message = input("Press enter to quit\n\n") # Run until someone presses enter
 
-    def on_tx_done(self):
-        print("\nTxDone")
-        print(self.get_irq_flags())
-
-    def on_cad_done(self):
-        print("\non_CadDone")
-        print(self.get_irq_flags())
-
-    def on_rx_timeout(self):
-        print("\non_RxTimeout")
-        print(self.get_irq_flags())
-
-    def on_valid_header(self):
-        print("\non_ValidHeader")
-        print(self.get_irq_flags())
-
-    def on_payload_crc_error(self):
-        print("\non_PayloadCrcError")
-        print(self.get_irq_flags())
-
-    def on_fhss_change_channel(self):
-        print("\non_FhssChangeChannel")
-        print(self.get_irq_flags())
-
-    def start(self):
-        while True:
-            self.reset_ptr_rx()
-            self.set_mode(MODE.RXCONT)  # Receiver mode
-            while True:
-                pass;
-
-lora = mylora(verbose=False)
-#args = parser.parse_args(lora) # configs in LoRaArgumentParser.py
-
-#     Slow+long range  Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. 13 dBm
-lora.set_pa_config(pa_select=1, max_power=21, output_power=15)
-lora.set_bw(BW.BW125)
-lora.set_coding_rate(CODING_RATE.CR4_8)
-lora.set_spreading_factor(12)
-lora.set_rx_crc(True)
-#lora.set_lna_gain(GAIN.G1)
-#lora.set_implicit_header_mode(False)
-lora.set_low_data_rate_optim(True)
-
-GPIO.setwarnings(False)  # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-GPIO.setmode(GPIO.BCM)
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-assert(lora.get_agc_auto_on() == 1)
-
-# while True:  # Run forever
-#     if GPIO.input(38) == GPIO.HIGH:
-#         print("Button off!")
-#     elif GPIO.input(40) == GPIO.HIGH:
-#         print("Button on!")
-
-try:
-    print("START")
-    lora.start()
-    if GPIO.input(38) == GPIO.HIGH:
-        print("Button off!")
-    elif GPIO.input(40) == GPIO.HIGH:
-        print("Button on!")
-except KeyboardInterrupt:
-    sys.stdout.flush()
-    print("Exit")
-    sys.stderr.write("KeyboardInterrupt\n")
-finally:
-    sys.stdout.flush()
-    print("Exit")
-    lora.set_mode(MODE.SLEEP)
-BOARD.teardown()
+GPIO.cleanup() # Clean up
